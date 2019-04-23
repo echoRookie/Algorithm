34a6be0d8a54ee2409bc2b95cc7a49bf65aae990
杨军 <1171349468@qq.com>
2019-04-23 18:35:58 +0800

    修改代码格式规范

--- src/src/Array/Test_26.java
+++ src/src/Array/Test_26.java
@@ -22,33 +22,32 @@ import java.io.Console;
 
 你不需要考虑数组中超出新长度后面的元素。 */
 
-public class Test_26 {
-	 public static int removeDuplicates(int[] nums) {
-	        int i;
-	        int k=1;
-	        int length = nums.length;
-	        int j;
-	        int temp;
-	        if(length <= 1){
-	            return length;
-	        }
-	        else{
-	             for(i = 1; i < length; i++){
-	                for (j=i;j<length;j++){
-	                    if(nums[j]>nums[i-1] && nums[j] > nums[k-1]){
-	                        temp = nums[i];
-	                        nums[i] = nums[j];
-	                        nums[j] = temp;
-	                        k += 1;
-	                        break;
-	                    }
-	                } 
-	               
-	             }
-	              return k;
-	        }
-	       
+public class Test_26{
+	public static int removeDuplicates(int[] nums){
+	    int i;
+	    int k=1;
+	    int length = nums.length;
+	    int j;
+	    int temp;
+	    if(length <= 1){
+	        return length;
 	    }
+        else{
+			for(i = 1; i < length; i++){
+			    for(j = i; j < length; j++){
+			        if(nums[j]>nums[i-1] && nums[j] > nums[k-1]){
+			            temp = nums[i];
+			            nums[i] = nums[j];
+			            nums[j] = temp;
+			            k += 1;
+			            break;
+			        }
+			    } 
+			   
+			 }
+			  return k;
+        } 
+	 }
 	 public static void main(String[] args){
 		 
 	
