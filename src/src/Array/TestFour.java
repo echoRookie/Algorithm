2628be26e38d14e1856e9bf184eb38eb4f10a595
杨军 <1171349468@qq.com>
2019-04-25 22:04:07 +0800

    feat：88. Merge Sorted Array

---
+++ src/src/Array/TestFour.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class TestFour {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) {
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+        while (n >= 0) {
+            nums1[p--] = nums2[n--];
+        }
+    }
+
+}
