60dca69e7631e1cca1cf91e29221d66651de800f
杨军 <1171349468@qq.com>
2020-10-27 23:01:52 +0800

    feat：新增题解

---
+++ src/InvertTree.java
@@ -0,0 +1,31 @@
+public class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+       if(root == null)
+            return root;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = right;
+        root.right = left;
+        invertTree(root.left);
+        invertTree(root.right);
+        return  root;
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
