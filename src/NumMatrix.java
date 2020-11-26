2610028653698fe456b50d5c0e1937a7daa5687b
echoRookie <1171349468@qq.com>
2020-11-26 15:32:44 +0800

    feat：新增题解304

---
+++ src/NumMatrix.java
@@ -0,0 +1,29 @@
+public class NumMatrix {
+    public static void main(String[] args) {
+        NumMatrix numMatrix = new NumMatrix(new int[][]{{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}});
+        System.out.println(numMatrix.sumRegion(2, 1, 4, 3));
+        System.out.println(numMatrix.sumRegion(1, 1, 2, 2));
+        System.out.println(numMatrix.sumRegion(1, 2, 2, 4));
+    }
+
+    public int[][] sum;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix == null || matrix.length <= 0) {
+            return;
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        sum = new int[rows + 1][columns + 1];
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2 + 1][col2 + 1] - sum[row1][col2 + 1] - sum[row2 + 1][col1] + sum[row1][col1];
+    }
+}
