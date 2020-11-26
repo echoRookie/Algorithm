5ee6e656133a61849b4592037ec68a2c85367d65
echoRookie <1171349468@qq.com>
2020-11-26 13:53:36 +0800

    feat：新增题解303

---
+++ src/NumArray.java
@@ -0,0 +1,18 @@
+/*
+ * 数组前缀和
+ * 将数组的前n项和存储到对应的区间，然后相减就是特定的区间和*/
+public class NumArray {
+    private int[] sum;
+
+    public NumArray(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+
+}
