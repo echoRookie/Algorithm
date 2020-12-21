814f596a8dedeb9d8e314708971dee5a6ac69784
echoRookie <1171349468@qq.com>
2020-12-21 15:49:30 +0800

    feat：新增题解217

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return true;
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        return false;
+    }
+}
