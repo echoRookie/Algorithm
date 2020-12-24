2a972b9b79c6db87d72c271d79b6305a1f544e1a
echoRookie <1171349468@qq.com>
2020-12-24 14:50:39 +0800

    feat:新增题解11

---
+++ src/MaxArea.java
@@ -0,0 +1,17 @@
+public class MaxArea {
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
+    public static int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int temp = (j - i) * Math.min(height[i], height[j]);
+                if (temp > max) {
+                    max = temp;
+                }
+            }
+        }
+        return max;
+    }
+}
