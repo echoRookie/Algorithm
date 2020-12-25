c26e9e902ecb617c1982de6c28beb640d27720fa
echoRookie <1171349468@qq.com>
2020-12-25 09:31:15 +0800

    feat：新增题解26

---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+    }
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}
